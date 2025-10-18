91c3b107d70b6359333ec0d7c2f80b2410e9532a
Lukas <lkoe@bluewin.ch>
2025-10-18 13:53:52 +0000

    Added mailhog to development pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,13 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  # Setup actionmailer to use mailhog
+  config.action_mailer.smtp_settings = {
+    address: "mailhog",
+    port: "1025"
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
