dc958ea2204c2a079ebcef40a55b4d6dea42b0d7
Lukas <lkoe@bluewin.ch>
2025-10-18 12:37:34 +0000

    Implemented notifications

---
+++ app/javascript/controllers/notification_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+import $ from "jquery";
+
+// Connects to data-controller="notification"
+export default class extends Controller {
+  close() {
+    $(this.element).remove();
+  }
+}
